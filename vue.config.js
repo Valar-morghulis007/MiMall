57c4906fe4ac79c01089a29a5b929c71b12de1ce
Valar-morghulis007 <1261644617@qq.com>
2024-02-19 00:09:28 +0800

    全局配置

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  devServer: {
+    host: "localhost",
+    port: 8081,
+    proxy: {
+      "/activity": {
+        target: "https://www.imooc.com",
+        changeOrigin: true,
+        pathRewrite:{
+          'activity':'activity'
+        }
+      },
+    },
+  },
+};
